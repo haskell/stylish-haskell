ae037aa1c03e63c66a003f02f30bceddd8de2297
Jasper Van der Jeugt <m@jaspervdj.be>
2012-05-25 11:44:30 +0200

    Stuff seems to work

--- draft/Imports.hs
+++ draft/Imports.hs
@@ -1,10 +1,16 @@
+import Control.Applicative ((<$>))
 import Language.Haskell.Exts.Annotated
 import System.Environment (getArgs)
 import Data.Ord (comparing)
+import Data.Maybe (isJust, maybeToList)
 import Data.List (sortBy)
 import Control.Arrow ((&&&))
 
+
+--------------------------------------------------------------------------------
 import Block
+import qualified Block
+import qualified Block as Herp
 
 
 --------------------------------------------------------------------------------
@@ -43,12 +49,49 @@ compareImports = comparing (importName &&& importQualified)
 
 
 --------------------------------------------------------------------------------
+prettyImport :: Int -> ImportDecl l -> String
+prettyImport longest imp = unwords $ concat
+    [ ["import"]
+    , [if importQualified imp then "qualified" else "         "]
+    , [(if hasExtras then padRight longest else id) (importName imp)]
+    , ["as " ++ as | ModuleName _ as <- maybeToList $ importAs imp]
+    , [prettyPrint specs | specs <- maybeToList $ importSpecs imp]
+    ]
+  where
+    hasExtras = isJust (importAs imp) || isJust (importSpecs imp)
+
+
+--------------------------------------------------------------------------------
+prettyImportGroup :: Int -> [ImportDecl Block] -> Lines
+prettyImportGroup longest = map (prettyImport longest) . sortBy compareImports
+
+
+--------------------------------------------------------------------------------
+padRight :: Int -> String -> String
+padRight len str = str ++ replicate (len - length str) ' '
+
+
+--------------------------------------------------------------------------------
+changes :: Module Block -> [Change]
+changes module' =
+    [ change block (prettyImportGroup longest importGroup)
+    | (block, importGroup) <- groups
+    ]
+  where
+    imps    = imports module'
+    longest = longestImport imps
+    groups  = groupAdjacent imps
+
+
+--------------------------------------------------------------------------------
 main :: IO ()
 main = do
     (filePath : _) <- getArgs
     parseResult    <- parseFile filePath
+    contents       <- lines <$> readFile filePath
     case parseResult of
-        ParseOk x       -> print $
-            map fst $ groupAdjacent $ imports $ fmap fromSrcSpanInfo x
+        ParseOk x       -> do
+            let module' = fmap fromSrcSpanInfo x
+            putStr $ unlines $ makeChanges (changes module') contents
         ParseFailed l s ->
             error $ "Parse failed: " ++ show l ++ " " ++ show s
