0acf5931a702dfcd7934b861e4ff1f64c96137dd
Jasper Van der Jeugt <m@jaspervdj.be>
2012-08-10 17:14:47 +0900

    A bit more drafting

--- src/StylishHaskell/Step/Records.hs
+++ src/StylishHaskell/Step/Records.hs
@@ -3,11 +3,13 @@ module StylishHaskell.Step.Records where
 
 
 --------------------------------------------------------------------------------
+import           Data.List                       (nub)
 import qualified Language.Haskell.Exts.Annotated as H
 
 
 --------------------------------------------------------------------------------
 import           StylishHaskell.Block
+import           StylishHaskell.Editor
 
 
 --------------------------------------------------------------------------------
@@ -21,10 +23,32 @@ records modu =
 
 
 --------------------------------------------------------------------------------
-fieldLines :: [H.FieldDecl H.SrcSpanInfo] -> Maybe [Int]
-fieldLines rec
-    | all singleLine blocks = Just $ map blockStart blocks
-    | otherwise             = Nothing
+-- | Align the types of a field
+{-
+alignType :: Int -> H.FieldDecl H.SrcSpan -> Int -> Change String
+alignType longest (H.FieldDecl _ names _) line = 
   where
-    blocks       = map (linesFromSrcSpan . H.ann) rec
-    singleLine b = blockStart b == blockEnd b
+    end = 
+-}
+
+
+--------------------------------------------------------------------------------
+-- | Find the length of the longest field name in a record
+longestFieldName :: [H.FieldDecl H.SrcSpan] -> Int
+longestFieldName fields = maximum
+    [ H.srcSpanEndColumn $ H.ann name
+    | H.FieldDecl _ names _ <- fields
+    , name                  <- names
+    ]
+
+
+--------------------------------------------------------------------------------
+-- | Checks that all no field of the record appears on more than one line,
+-- amonst other things
+fixable :: [H.FieldDecl H.SrcSpan] -> Bool
+fixable []     = False
+fixable fields = all singleLine srcSpans && nonOverlapping srcSpans
+  where
+    srcSpans          = map H.ann fields
+    singleLine s      = H.srcSpanStartLine s == H.srcSpanEndLine s
+    nonOverlapping ss = length ss == length (nub $ map H.srcSpanStartLine ss)
