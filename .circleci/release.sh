e3fe396bf447b0419ea1a9a45c1c56f993e24306
Jasper Van der Jeugt <m@jaspervdj.be>
2019-10-29 13:21:42 +0100

    Bump release script

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -20,13 +20,13 @@ fi
 
 # Install ghr
 GHR_VERSION="v0.5.4"
-wget --quiet \
+curl -O \
     "https://github.com/tcnksm/ghr/releases/download/${GHR_VERSION}/ghr_${GHR_VERSION}_linux_386.zip"
 unzip ghr_${GHR_VERSION}_linux_386.zip
 
 # Install upx
 UPX_VERSION="3.94"
-wget --quiet \
+curl -O \
     "https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-amd64_linux.tar.xz"
 tar xf upx-${UPX_VERSION}-amd64_linux.tar.xz
 mv upx-${UPX_VERSION}-amd64_linux/upx .
