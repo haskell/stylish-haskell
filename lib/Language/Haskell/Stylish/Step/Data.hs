fd44a32c0c80c6bc3721788a63c443ae3e4fb6dd
Paweł Szulc <paul.szulc@gmail.com>
2020-01-16 17:26:06 +0100

    Cover case where there are more then one field in data type declaration
    Co-authored-by: Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>

--- lib/Language/Haskell/Stylish/Step/Data.hs
+++ lib/Language/Haskell/Stylish/Step/Data.hs
@@ -1,7 +1,7 @@
 module Language.Haskell.Stylish.Step.Data where
 
+import           Data.Maybe                      (maybeToList)
 import qualified Language.Haskell.Exts           as H
-
 import           Language.Haskell.Stylish.Block
 import           Language.Haskell.Stylish.Editor
 import           Language.Haskell.Stylish.Step
@@ -22,17 +22,15 @@ step' :: Lines -> Module -> Lines
 step' ls (module', _) = applyChanges changes ls
   where
     datas' = datas $ fmap linesFromSrcSpan module'
-    changes = datas' >>= changeDecl
+    changes = datas' >>= (maybeToList . changeDecl)
 
-changeDecl :: (LineBlock, H.Decl l)  -> [ChangeLine]
-changeDecl (block, H.DataDecl _ (H.DataType _) _ (H.DHead _ ident) qualConDecls _) = do
-  (H.QualConDecl _ _ _ (H.RecDecl _ dname fields)) <- qualConDecls
-  (H.FieldDecl _ names _type) <- fields
-  fname <- names
-  [change block (\_ ->
-                   ["data " <> H.prettyPrint ident <> " = " <> H.prettyPrint dname
-                   ,"  { " <> H.prettyPrint fname <> " :: " <> H.prettyPrint _type
-                   ,"  }"
-                   ]
-                )]
-changeDecl _ = []
+changeDecl :: (LineBlock, H.Decl l)  -> Maybe ChangeLine
+changeDecl (block, H.DataDecl _ (H.DataType _) _ (H.DHead _ ident) [(H.QualConDecl _ _ _ (H.RecDecl _ dname fields))] _) =
+  Just $ change block (const newLines)
+  where
+    newLines = typeConstructor : (firstName $ extractField $ head fields) : (fmap (otherName . extractField) (tail fields)) ++ ["  }"]
+    typeConstructor = "data " <> H.prettyPrint ident <> " = " <> H.prettyPrint dname
+    firstName (fname, _type) = "  { " <> H.prettyPrint fname <> " :: " <> H.prettyPrint _type
+    otherName (fname, _type) = "  , " <> H.prettyPrint fname <> " :: " <> H.prettyPrint _type
+    extractField (H.FieldDecl _ names _type) = (head names, _type)
+changeDecl _ = Nothing
--- tests/Language/Haskell/Stylish/Step/Data/Tests.hs
+++ tests/Language/Haskell/Stylish/Step/Data/Tests.hs
@@ -10,22 +10,40 @@ import           Test.HUnit                          (Assertion, (@=?))
 
 tests :: Test
 tests = testGroup "Language.Haskell.Stylish.Step.Data.Tests"
-    [ testCase "case 01" case01]
-
-simpleInput :: String
-simpleInput = unlines
-  [ "module Herp where"
-  , ""
-  , "data Foo = Foo { a :: Int }"
-  ]
+    [ testCase "case 01" case01
+    , testCase "case 02" case02
+    ]
 
 case01 :: Assertion
-case01 = expected @=? testStep step simpleInput
+case01 = expected @=? testStep step input
   where
+    input = unlines
+      [ "module Herp where"
+      , ""
+      , "data Foo = Foo { a :: Int }"
+      ]
+
     expected = unlines
        [ "module Herp where"
        , ""
        , "data Foo = Foo"
        , "  { a :: Int"
+       , "  }"
+       ]
+
+case02 :: Assertion
+case02 = expected @=? testStep step input
+  where
+    input = unlines
+      [ "module Herp where"
+      , ""
+      , "data Foo = Foo { a :: Int, a2 :: String }"
+      ]
+    expected = unlines
+       [ "module Herp where"
+       , ""
+       , "data Foo = Foo"
+       , "  { a :: Int"
+       , "  , a2 :: String"
        , "  }"
        ]
