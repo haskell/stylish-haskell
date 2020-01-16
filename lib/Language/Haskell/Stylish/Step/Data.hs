43718c19c4e03dd8f3c390ca481504eb49c91cbf
Lukasz Golebiewski <lukasz.golebiewski@gmail.com>
2020-01-16 13:07:04 +0100

    [sanity-check] Delete data defs

--- lib/Language/Haskell/Stylish/Step/Data.hs
+++ lib/Language/Haskell/Stylish/Step/Data.hs
@@ -1,6 +1,28 @@
 module Language.Haskell.Stylish.Step.Data where
 
+import qualified Language.Haskell.Exts           as H
+import qualified Language.Haskell.Exts.Syntax    as H
+
+import           Language.Haskell.Stylish.Block
+import           Language.Haskell.Stylish.Editor
 import           Language.Haskell.Stylish.Step
+import           Language.Haskell.Stylish.Util
+
+datas :: H.Module l -> [(l, H.Decl l)]
+datas modu =
+    [ (l, H.DataDecl l b c d e f)
+    | H.Module _ _ _ _ decls                  <- [modu]
+    , H.DataDecl l b c d e f                  <- decls
+    ]
+
 
 step :: Step
-step = makeStep "Data" const
+step = makeStep "Data" step'
+
+step' :: Lines -> Module -> Lines
+step' ls (module', _) = applyChanges changes ls
+  where
+    datas' = datas $ fmap linesFromSrcSpan module'
+    changes = fmap (delete . fst) datas'
+
+prettyDataDecls = undefined
