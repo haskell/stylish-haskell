3b9500c72aa68c765d82186bf3d6fdc99199c89f
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2020-01-17 14:50:51 +0100

    Apply hlint suggestions

--- lib/Language/Haskell/Stylish/Step/Data.hs
+++ lib/Language/Haskell/Stylish/Step/Data.hs
@@ -1,13 +1,13 @@
 module Language.Haskell.Stylish.Step.Data where
 
-import           Prelude hiding (init)
-import           Data.List (intercalate)
+import           Data.List                       (intercalate)
 import           Data.Maybe                      (maybeToList)
 import qualified Language.Haskell.Exts           as H
 import           Language.Haskell.Stylish.Block
 import           Language.Haskell.Stylish.Editor
 import           Language.Haskell.Stylish.Step
 import           Language.Haskell.Stylish.Util
+import           Prelude                         hiding (init)
 
 datas :: H.Module l -> [(l, H.Decl l)]
 datas modu =
@@ -25,7 +25,7 @@ step' :: Int -> Lines -> Module -> Lines
 step' indentSize ls (module', _) = applyChanges changes ls
   where
     datas' = datas $ fmap linesFromSrcSpan module'
-    changes = datas' >>= (maybeToList . (changeDecl indentSize))
+    changes = datas' >>= maybeToList . changeDecl indentSize
 
 changeDecl :: Int -> (LineBlock, H.Decl l)  -> Maybe ChangeLine
 changeDecl _ (_, H.DataDecl _ (H.DataType _) Nothing _ [] _) = Nothing
@@ -34,15 +34,15 @@ changeDecl indentSize (block, H.DataDecl _ (H.DataType _) Nothing dhead decls de
   where
     zipped = zip decls ([1..] ::[Int])
     newLines :: [[String]]
-    newLines = (fmap (\(decl, i) -> if (i == 1) then processConstructor typeConstructor decl else processConstructor (indented "| ") decl) zipped) ++ [fmap (\d -> indented $ H.prettyPrint d) derivings]
+    newLines = fmap (\(decl, i) -> if i == 1 then processConstructor typeConstructor decl else processConstructor (indented "| ") decl) zipped ++ [fmap (indented . H.prettyPrint) derivings]
     typeConstructor = "data " <> H.prettyPrint dhead <> " = "
     processName init (fnames, _type) = indented init <> intercalate ", " (fmap H.prettyPrint fnames) <> " :: " <> H.prettyPrint _type
     extractField (H.FieldDecl _ names _type) = (names, _type)
 
-    processConstructor init (H.QualConDecl _ _ _ (H.RecDecl _ dname fields)) = do
-      init <> H.prettyPrint dname : (processName "{ " $ extractField $ head fields) : (fmap (processName ", " . extractField) (tail fields)) ++ [indented "}"]
+    processConstructor init (H.QualConDecl _ _ _ (H.RecDecl _ dname fields)) =
+      init <> H.prettyPrint dname : processName "{ " ( extractField $ head fields) : fmap (processName ", " . extractField) (tail fields) ++ [indented "}"]
     processConstructor init decl = [init <> trimLeft (H.prettyPrint decl)]
 
-    indented str = indent indentSize str
+    indented = indent indentSize
 
 changeDecl _ _ = Nothing
