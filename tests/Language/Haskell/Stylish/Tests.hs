ec66e577571721ae9841d84a5603d69bc45b214e
Maxim Koltsov <kolmax94@gmail.com>
2020-02-01 12:03:36 +0300

    Disable formatting of data types without records
    Perhaps the safest way for now is to revert to old behavior for data
    types -- i.e. do nothing. Attemmpt reformatting only if at least one
    constructor has record fields.
    
    Fixes https://github.com/jaspervdj/stylish-haskell/issues/262

--- tests/Language/Haskell/Stylish/Tests.hs
+++ tests/Language/Haskell/Stylish/Tests.hs
@@ -28,10 +28,9 @@ tests = testGroup "Language.Haskell.Stylish.Step.Tabs.Tests"
 case01 :: Assertion
 case01 = (@?= result) =<< format Nothing Nothing input
   where
-    input = "module Herp where\n data Foo = Bar | Baz"
+    input = "module Herp where\ndata Foo = Bar | Baz"
     result = Right [ "module Herp where"
-                   , "data Foo = Bar"
-                   , "    | Baz"
+                   , "data Foo = Bar | Baz"
                    ]
 
 
@@ -47,10 +46,9 @@ case02 = withTestDirTree $ do
     actual <- format (Just $ ConfigPath "test-config.yaml") Nothing input
     actual @?= result
   where
-    input = "module Herp where\n data Foo = Bar | Baz"
+    input = "module Herp where\ndata Foo = Bar | Baz"
     result = Right [ "module Herp where"
-                   , "data Foo = Bar"
-                   , "  | Baz"
+                   , "data Foo = Bar | Baz"
                    ]
 
 
