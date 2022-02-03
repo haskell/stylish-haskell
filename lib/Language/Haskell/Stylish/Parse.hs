c67858030e9cf6146a9d20c753ba0a395114402a
Jasper Van der Jeugt <m@jaspervdj.be>
2022-02-03 14:42:42 +0100

    Grab language pragmas and dynflag them

--- lib/Language/Haskell/Stylish/Parse.hs
+++ lib/Language/Haskell/Stylish/Parse.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
 --------------------------------------------------------------------------------
 module Language.Haskell.Stylish.Parse
   ( parseModule
@@ -6,29 +5,30 @@ module Language.Haskell.Stylish.Parse
 
 
 --------------------------------------------------------------------------------
-import           Data.Maybe                                 (fromMaybe, listToMaybe, mapMaybe)
-import Data.List (stripPrefix)
-import Control.Monad ((>=>))
-import qualified GHC.Data.StringBuffer                      as GHC
-import qualified GHC.Hs                                     as GHC
-import qualified GHC.Types.SrcLoc                           as GHC
-import qualified GHC.Driver.Config                          as GHC
-import           GHC.Driver.Ppr                             as GHC
-import qualified GHC.Driver.Session                         as GHC
-import qualified GHC.LanguageExtensions.Type                as LangExt
-import qualified GHC.Parser.Errors.Ppr                      as GHC
-import qualified GHC.Parser.Lexer                           as GHC
-import qualified GHC.Parser.Header                          as GHC
-import qualified GHC.Utils.Error                            as GHC
-import qualified GHC.Utils.Outputable                       as GHC
-import qualified Language.Haskell.GhclibParserEx.GHC.Parser as GHCEx
+import           Control.Monad                                      ((>=>))
+import           Data.List                                          (foldl',
+                                                                     stripPrefix)
+import           Data.Maybe                                         (fromMaybe,
+                                                                     listToMaybe,
+                                                                     mapMaybe)
+import           Data.Traversable                                   (for)
+import qualified GHC.Data.StringBuffer                              as GHC
+import           GHC.Driver.Ppr                                     as GHC
+import qualified GHC.Driver.Session                                 as GHC
+import qualified GHC.LanguageExtensions.Type                        as LangExt
+import qualified GHC.Parser.Errors.Ppr                              as GHC
+import qualified GHC.Parser.Header                                  as GHC
+import qualified GHC.Parser.Lexer                                   as GHC
+import qualified GHC.Types.SrcLoc                                   as GHC
+import qualified GHC.Utils.Error                                    as GHC
+import qualified GHC.Utils.Outputable                               as GHC
 import qualified Language.Haskell.GhclibParserEx.GHC.Driver.Session as GHCEx
+import qualified Language.Haskell.GhclibParserEx.GHC.Parser         as GHCEx
 
 
 --------------------------------------------------------------------------------
-import           Language.Haskell.Stylish.GHC               (baseDynFlags, showOutputable)
+import           Language.Haskell.Stylish.GHC                       (baseDynFlags)
 import           Language.Haskell.Stylish.Module
-import Debug.Trace
 
 
 type Extensions = [String]
@@ -57,29 +57,36 @@ dropBom str              = str
 --------------------------------------------------------------------------------
 -- | Abstraction over GHC lib's parsing
 parseModule :: Extensions -> Maybe FilePath -> String -> Either String Module
-parseModule externalExts fp string =
-    let input = removeCpp $ dropBom string in
-    (trace ("options: " ++ show (mapMaybe (optionToPragma . GHC.unLoc) parseOptions))) $
-    case GHCEx.parseModule input dynFlags of
-      GHC.POk _ m -> Right m
-      GHC.PFailed ps -> Left . withFileName . GHC.showSDoc dynFlags . GHC.vcat .
-          GHC.pprMsgEnvelopeBagWithLoc . fmap GHC.pprError . snd $
-          GHC.getMessages ps
+parseModule externalExts0 fp string = do
+    -- Parse extensions.
+    externalExts1 <- for externalExts0 $ \s -> case GHCEx.readExtension s of
+        Nothing -> Left $ "Unknown extension: " ++ show s
+        Just e  -> Right e
+
+    -- Build first dynflags.
+    let dynFlags0 = foldl' GHC.xopt_set baseDynFlags externalExts1
+
+    -- Parse options from file
+    let fileOptions = fmap GHC.unLoc $ GHC.getOptions dynFlags0
+            (GHC.stringToStringBuffer string)
+            (fromMaybe "-" fp)
+        fileExtensions = mapMaybe
+            (stripPrefix "-X" >=> GHCEx.readExtension)
+            fileOptions
+
+    -- Set further dynflags.
+    let dynFlags1 = foldl' GHC.xopt_set dynFlags0 fileExtensions
+            `GHC.gopt_set` GHC.Opt_KeepRawTokenStream
+
+    -- Possibly strip CPP.
+    let removeCpp s = if GHC.xopt LangExt.Cpp dynFlags1 then unCpp s else s
+        input = removeCpp $ dropBom string
+
+    -- Actual parse.
+    case GHCEx.parseModule input dynFlags1 of
+        GHC.POk _ m -> Right m
+        GHC.PFailed ps -> Left . withFileName . GHC.showSDoc dynFlags1 .
+            GHC.vcat . GHC.pprMsgEnvelopeBagWithLoc . fmap GHC.pprError . snd $
+            GHC.getMessages ps
   where
-    -- TODO: Add extensions again.
-    dynFlags0 = foldl'
-        (\flags extStr -> case GHCEx.readExtension extStr of
-            Nothing -> flags)
-        baseDynFlags
-        externalExts
-        `GHC.gopt_set` GHC.Opt_KeepRawTokenStream
-
-    parseOptions = GHC.getOptions dynFlags0
-        (GHC.stringToStringBuffer string)
-        (fromMaybe "-" fp)
-
-    removeCpp s = if GHC.xopt LangExt.Cpp dynFlags then unCpp s else s
-
     withFileName x = maybe "" (<> ": ") fp <> x
-
-    optionToPragma = stripPrefix "-X" >=> GHCEx.readExtension
