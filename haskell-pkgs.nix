9b919fee2542649b6f268e4e93fb32699eb1bf98
Tikhon Jelvis <tikhon@jelv.is>
2022-04-18 16:39:51 -0700

    Fix Nix build, bumping haskell.nix (#397)
    Fixes `nix-shell` and `nix-build` for the repo, which was failing with a dependency error (#397).
    
    This PR takes two steps to fix this:
    
      1. It bumps the version of `haskell.nix` we use, switching to GHC 8.10.7 and Nixpkgs 21.11 so that we still use the haskell.nix cache
    
      2. After 1., I ran into an error where haskell.nix was using Cabal-3.2.1.0 even though `stylish-haskell.cabal` needs Cabal >= 3.4. To fix this, I had to explicitly mark the Cabal library as "reinstallable"—otherwise, haskell.nix uses the exact version that comes with GHC. (See [haskell-nix issue #1337][1] for details.)
    
    [1]: https://github.com/input-output-hk/haskell.nix/issues/1337
    
    I tested this change locally. Both `nix-build` and `nix-shell` succeeded without needing to build GHC from source.

--- haskell-pkgs.nix
+++ haskell-pkgs.nix
@@ -2,14 +2,14 @@ let
   # Fetch the latest haskell.nix and import its default.nix
   haskellNix = import
     (builtins.fetchTarball {
-      url = "https://github.com/input-output-hk/haskell.nix/archive/1b9b05beed75a1be98405501fbb33ac1f080069e.tar.gz";
+      url = "https://github.com/input-output-hk/haskell.nix/archive/cc40a24585ccba274dc9a5af96d5506034e0d658.tar.gz";
     })
     { };
 
   # haskell.nix provides access to the nixpkgs pins which are used by our CI,
   # hence you will be more likely to get cache hits when using these.
   # But you can also just use your own, e.g. '<nixpkgs>'.
-  nixpkgsSrc = haskellNix.sources.nixpkgs-2009;
+  nixpkgsSrc = haskellNix.sources.nixpkgs-2111;
 
   # haskell.nix provides some arguments to be passed to nixpkgs, including some
   # patches and also the haskell.nix functionality itself as an overlay.
