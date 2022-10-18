6cc2544adeda06218f9deb35c9aafa98adf2739f
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2022-10-19 01:15:26 +0900

    support a new style of the GitHub releases page

--- scripts/latest.sh
+++ scripts/latest.sh
@@ -7,7 +7,9 @@ PACKAGE=stylish-haskell
 echo Downloading and running $PACKAGE...
 
 RELEASES=$(curl --silent https://github.com/haskell/$PACKAGE/releases)
-URL=https://github.com/$(echo $RELEASES | grep -o '\"[^\"]*-linux-x86_64\.tar\.gz\"' | sed s/\"//g | head -n1)
+ASSETS_URL=https://github.com/haskell/stylish-haskell/releases/expanded_assets/$(echo $RELEASES | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | head -n1)
+ASSETS=$(curl --silent $ASSETS_URL)
+URL=https://github.com/$(echo $ASSETS | grep -o '\"[^\"]*-linux-x86_64\.tar\.gz\"' | sed s/\"//g | head -n1)
 VERSION=$(echo $URL | sed -e 's/.*-\(v[\.0-9]\+-linux-x86_64\)\.tar\.gz/\1/')
 TEMP=$(mktemp --directory .$PACKAGE-XXXXX)
 
