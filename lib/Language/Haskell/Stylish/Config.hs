cbc2f46d213b636b0ebccf591cc2ce3c68301a86
Jan Hrček <honza.hrk@gmail.com>
2024-06-27 09:46:05 +0200

    Improve haddocks, add changelog entry

--- lib/Language/Haskell/Stylish/Config.hs
+++ lib/Language/Haskell/Stylish/Config.hs
@@ -100,7 +100,8 @@ data ConfigSearchStrategy
     = -- | Don't try to search, just use given config file
       UseConfig FilePath
     | -- | Search for @.stylish-haskell.yaml@ starting from given directory.
-      -- If not found, try all ancestor directories, @$XDG_CONFIG/stylish-haskell/config.yaml@ and @$HOME/.stylish-haskell.yaml@ in order
+      -- If not found, try all ancestor directories, @$XDG_CONFIG\/stylish-haskell\/config.yaml@ and @$HOME\/.stylish-haskell.yaml@ in order.
+      -- If no config is found, default built-in config will be used.
       SearchFromDirectory FilePath
     | -- | Like SearchFromDirectory, but using current working directory as a starting point
       SearchFromCurrentDirectory
